b51ef86bd810af270c64d3c044f85ee9fd4e2334
erwanmess <erwan.messaglio@edu.itescia.fr>
2020-10-23 15:14:25 +0200

    nouvelle modif

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+    @FXML
+    private void Aboutus (ActionEvent Event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("trombi.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Trombinoscope");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch (Exception e){System.out.println("raté");}
+
+    }
 }
