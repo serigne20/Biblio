2793199b6f3d95a0710b9e1bff29f591d61ecfc0
ericguillet <eric.guillet@edu.itescia.fr>
2020-10-23 16:27:14 +0200

    ajout popup CloseApp

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,12 +17,17 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Bibliothéque");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 650, 475));
         primaryStage.show();
     }
 
-
+    @FXML
+    public void CloseApp(ActionEvent event){
+        Platform.exit();
+        System.exit(0);
+    }
     public static void main(String[] args) {
         launch(args);
     }
+
 }
