ffcfbf48c0c87adedcd49dced719d62cd02b6169
ericguillet <eric.guillet@edu.itescia.fr>
2020-10-27 20:18:04 +0100

    Utilisation d'un fichier XML

--- src/main/java/JaxbTests/Bibliotheque.java
+++ src/main/java/JaxbTests/Bibliotheque.java
@@ -276,6 +276,7 @@ public class Bibliotheque {
         }
 
 
+
         /**
          * <p>Classe Java pour anonymous complex type.
          * 
--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import JaxbTests.Bibliotheque;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -26,6 +27,9 @@ import org.w3c.dom.NodeList;
 
 import javax.annotation.PostConstruct;
 import javax.swing.*;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -38,16 +42,16 @@ import java.util.stream.IntStream;
 
 
 public class Controller implements Initializable {
-    private ObservableList<Livre> livres = FXCollections.observableArrayList();
+    private ObservableList<Bibliotheque.Livre> livres = FXCollections.observableArrayList();
     private int Livreindex;
     @FXML private javafx.scene.control.MenuItem CloseAppButton;
-    @FXML private TableView<Livre> tableBook;
-    @FXML private TableColumn<Livre, String> TitreColumn;
-    @FXML private TableColumn<Livre, String> AuteurColumn;
-    @FXML private TableColumn<Livre, String> ResumeColumn;
-    @FXML private TableColumn<Livre, Integer> ColonneColumn;
-    @FXML private TableColumn<Livre, Integer> RangeeColumn;
-    @FXML private TableColumn<Livre, String> ParutionColumn;
+    @FXML private TableView<Bibliotheque.Livre> tableBook;
+    @FXML private TableColumn<Bibliotheque.Livre, String> TitreColumn;
+    @FXML private TableColumn<Bibliotheque.Livre, String> AuteurColumn;
+    @FXML private TableColumn<Bibliotheque.Livre, String> ResumeColumn;
+    @FXML private TableColumn<Bibliotheque.Livre, Integer> ColonneColumn;
+    @FXML private TableColumn<Bibliotheque.Livre, Integer> RangeeColumn;
+    @FXML private TableColumn<Bibliotheque.Livre, String> ParutionColumn;
     @FXML private TextField TitreInput;
     @FXML private TextField AuteurInput;
     @FXML private TextField ParutionInput;
@@ -63,26 +67,25 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Mise en place des colonnes du tableau
-        TitreColumn.setCellValueFactory(new PropertyValueFactory<Livre, String>("Titre"));
-        AuteurColumn.setCellValueFactory(new PropertyValueFactory<Livre, String>("Auteur"));
-        ResumeColumn.setCellValueFactory(new PropertyValueFactory<Livre, String>("Resume"));
-        ColonneColumn.setCellValueFactory(new PropertyValueFactory<Livre, Integer>("Colonne"));
-        RangeeColumn.setCellValueFactory(new PropertyValueFactory<Livre, Integer>("Rangee"));
-        ParutionColumn.setCellValueFactory(new PropertyValueFactory<Livre, String>("Parution"));
+        TitreColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, String>("Titre"));
+        AuteurColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, String>("Auteur"));
+        ResumeColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, String>("Presentation"));
+        ColonneColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, Integer>("Colonne"));
+        RangeeColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, Integer>("Rangee"));
+        ParutionColumn.setCellValueFactory(new PropertyValueFactory<Bibliotheque.Livre, String>("Parution"));
         disableInput();
-        //loadXMLFile();
         //Mise en place d'un OnMouseClickedEvent afin d'avoir les données du tableau
         tableBook.setRowFactory(tv -> {
-            TableRow<Livre> row = new TableRow<>();
+            TableRow<Bibliotheque.Livre> row = new TableRow<>();
             row.setOnMouseClicked(event ->{
                 if(!row.isEmpty()){
-                    Livre rowData = row.getItem();
+                    Bibliotheque.Livre rowData = row.getItem();
                     TitreInput.setText(rowData.getTitre());
-                    AuteurInput.setText(rowData.getAuteur());
+                    AuteurInput.setText(rowData.getAuteur().getPrenom()+" "+rowData.getAuteur().getNom());
                     ParutionInput.setText(String.valueOf(rowData.getParution()));
                     ColonneInput.setText(String.valueOf(rowData.getColonne()));
                     RangeeInput.setText(String.valueOf(rowData.getRangee()));
-                    ResumeInput.setText(rowData.getResume());
+                    ResumeInput.setText(rowData.getPresentation());
                     Livreindex = row.getIndex();
                 }
             });
@@ -94,14 +97,14 @@ public class Controller implements Initializable {
      * getLivres(Livre) permet l'ajout d'un livre dans une liste.
      * return ObservableList<Livre> permet de retourner une liste de livre qui sera ajouter ensuite dans notre TableView.
      */
-    public ObservableList<Livre> getLivre(Livre l){
+    public ObservableList<Bibliotheque.Livre> getLivre(Bibliotheque.Livre l){
         livres.add(l);
         return livres;
     }
 
     /**
      * Cette méthode permet d'afficher le menu de confirmation de l'arrêt de l'application.
-     * @param event sert à exectuer la méthode se trouvant dans le onAction de notre fichier sample.fxml.
+     * @param event sert à executer la méthode se trouvant dans le onAction de notre fichier sample.fxml.
      * Ce qui permet d'activer la méthode lors d'un clique.
      */
     public void ShowCloseAppMenu(ActionEvent event) {
@@ -145,20 +148,23 @@ public class Controller implements Initializable {
     private void validerLivre (ActionEvent Event){
         String prenom, nom= "";
         String titre = TitreInput.getText();
-        String paru=ParutionInput.getText();
         String res=ResumeInput.getText();
         String aut=AuteurInput.getText();
-        try{int c =Integer.parseInt(ColonneInput.getText());
+        try{
+            int c =Integer.parseInt(ColonneInput.getText());
+            int paru=Integer.parseInt(ParutionInput.getText());
             int r =Integer.parseInt(RangeeInput.getText());
             if (c<=5 && c>=1 && r<=7 && r>=1){
                 if (TitreInput.getText().isEmpty()){
                     titre= "Titre incconu";
                 }
-                if (AuteurInput.getText().isEmpty()){
-                    aut="auteur inconnu";
-                }
-                if (ParutionInput.getText().isEmpty()){
-                    paru="parution inconnu";
+                if (AuteurInput.getText().indexOf(" ")==-1){
+                    if (AuteurInput.getText().isEmpty()){
+                        aut="auteur inconnu";
+                    }
+                    else{
+                        aut=" "+AuteurInput.getText();
+                    }
                 }
                 if (ResumeInput.getText().isEmpty()){
                     res="résumé vide";
@@ -166,7 +172,17 @@ public class Controller implements Initializable {
                 String[] auteur= aut.split(" ");
                 prenom=auteur[1];
                 nom=auteur[0];
-                Livre l1 = new Livre (titre,nom,prenom,paru, c,r, res);
+                Bibliotheque.Livre l1 = new Bibliotheque.Livre();
+                Bibliotheque.Livre.Auteur auteur1 = new Bibliotheque.Livre.Auteur();
+                auteur1.setNom(nom);
+                auteur1.setPrenom(prenom);
+                l1.setAuteur(auteur1);
+                l1.setTitre(titre);
+                l1.setColonne((short)c);
+                l1.setParution(paru);
+                l1.setPresentation(res);
+                l1.setRangee((short) r);
+
                 tableBook.setItems(getLivre(l1));
                 disableInput();
                 resetInput();
@@ -180,6 +196,11 @@ public class Controller implements Initializable {
         }
 
     }
+
+    /**
+     * Permet l'ouverture de l'explorateur de fichier afin de choisir un fichier XML
+     * @param actionEvent ce paramètre permet l'action de cliquer sur le bouton et d'excuter la méthode.
+     */
     @FXML
     public void Open(javafx.event.ActionEvent actionEvent) {
 
@@ -188,8 +209,13 @@ public class Controller implements Initializable {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("xml files", "*.XML"));
         File selectedFile = fileChooser.showOpenDialog(null);
+        loadXMLFile(selectedFile);
 
     }
+
+    /**
+     * Affiche un message d'erreur lorsque les valeurs limites de colonne ou rangée
+     */
     @FXML
     public void erreur() {
         try {
@@ -203,6 +229,9 @@ public class Controller implements Initializable {
         catch (Exception e){System.out.println("raté");}
     }
 
+    /**
+     * Permet d'empêcher l'écriture dans les inputs tant que l'utilisateur n'a pas appuyer sur le bouton Ajouter
+     */
     public void disableInput(){
         TitreInput.setDisable(true);
         AuteurInput.setDisable(true);
@@ -211,6 +240,10 @@ public class Controller implements Initializable {
         RangeeInput.setDisable(true);
         ParutionInput.setDisable(true);
     }
+
+    /**
+     * Permet l'écriture dans les inputs afin d'ajouter ou modifier un livre
+     */
     public void enableInput(){
         TitreInput.setDisable(false);
         AuteurInput.setDisable(false);
@@ -220,6 +253,10 @@ public class Controller implements Initializable {
         ParutionInput.setDisable(false);
         resetInput();
     }
+
+    /**
+     * Permet le reste des inputs afin de ne pas ajouter 2 livres similaires
+     */
     public void resetInput(){
         TitreInput.setText("");
         AuteurInput.setText("");
@@ -235,27 +272,25 @@ public class Controller implements Initializable {
         String aut=AuteurInput.getText();
         int c =Integer.parseInt(ColonneInput.getText());
         int r =Integer.parseInt(RangeeInput.getText());
-        Livre l =  new Livre(titre,aut,paru,c,r,res);
         livres.remove(Livreindex);
         resetInput();
     }
-    public void loadXMLFile() {
+
+    /**
+     * Permet d'ouvrir un fichier XML et de compléter le tableau avec les données de celui-ci
+     * @param selectedFile permet de récupérer le nom du fichier choisi par l'utilisateur grâce à la méthode Open()
+     */
+    public void loadXMLFile(File selectedFile) {
+        JAXBContext jc = null;
         try {
-            File xmlFile = new File("/biblio/Biblio.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
-            NodeList biblioNodeList = doc.getElementsByTagName("bibliotheque");
-            for (int parameter = 0; parameter < biblioNodeList.getLength(); parameter++) {
-                Livre l = new Livre();
-                Node node = biblioNodeList.item(parameter);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) node;
-                    String titre = eElement.getElementsByTagName("titre").item(0).getTextContent();
-                    String parution = eElement.getElementsByTagName("parution").item(0).getTextContent();
-                    System.out.println(parution + " " + titre);
-                }
+            jc = JAXBContext.newInstance("JaxbTests");
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            Bibliotheque bibliotheque = (Bibliotheque) unmarshaller.unmarshal(selectedFile);
+            List<Bibliotheque.Livre> list = bibliotheque.getLivre();
+            for (int i = 0; i < list.size(); i++) {
+                Bibliotheque.Livre livre = list.get(i);
+                livres.add(livre);
+                tableBook.setItems(livres);
             }
         } catch (Exception e) {
             System.out.println("raté");
