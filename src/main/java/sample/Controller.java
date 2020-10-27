13dacba427bf5cba5d009ca601e6bfd6d6bc815b
erwanmess <erwan.messaglio@edu.itescia.fr>
2020-10-27 12:40:54 +0100

    Controller 27/10

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -15,10 +15,6 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-
-
-
 public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
